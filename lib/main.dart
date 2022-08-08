82758d9704377c04b4081b804f984ce4e02fd852
ryan2156 <adasdsd60112@gmail.com>
2022-08-08 17:18:14 +0800

    start

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+   home: Scaffold(
+     appBar: AppBar(
+       title: const Text('客戶訂單系統'),
+       centerTitle: false,
+       backgroundColor: Colors.green,
+       titleSpacing: 30,
+     ),
+   ),
+  ));
+}
+
+
+
